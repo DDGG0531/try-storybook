aa8361635fe49321f1fc1183585ef4de847f00f6
JimDong <jimdong@damaiapp.com.tw>
2022-11-09 10:58:25 +0800

    refc

--- src/myStory/Twitter-Like-Button.stories.tsx
+++ src/myStory/Twitter-Like-Button.stories.tsx
@@ -1,21 +1,21 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+
 import TwitterLikeButton from "./Twitter-Like-Button";
 
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Jim/TwitterLikeButton",
   component: TwitterLikeButton,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof TwitterLikeButton>;
 
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof TwitterLikeButton> = (args) => (
   <TwitterLikeButton {...args} />
 );
 
-export const Animate = Template.bind({});
-Animate.args = {
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
   canAnimate: true,
-};
-
-export const NoAnimate = Template.bind({});
-NoAnimate.args = {
-  canAnimate: false,
 };
